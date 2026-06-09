a612553940d3c6a3dfd9e674906692b469757a0f
Houssein372021 <hseinghannoum@gmail.com>
2026-06-09 23:14:34 +0200

    Ajouter le repository des candidatures

---
+++ backend/src/main/java/com/jobpilot/backend/jobapplication/repository/JobApplicationRepository.java
@@ -0,0 +1,19 @@
+package com.jobpilot.backend.jobapplication.repository;
+
+import com.jobpilot.backend.jobapplication.entity.ApplicationStatus;
+import com.jobpilot.backend.jobapplication.entity.JobApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import java.util.List;
+import java.util.UUID;
+
+public interface JobApplicationRepository extends JpaRepository<JobApplication, UUID> {
+
+    List<JobApplication> findByUserIdOrderByCreatedAtDesc(UUID userId);
+
+    List<JobApplication> findByUserIdAndStatusOrderByCreatedAtDesc(
+            UUID userId,
+            ApplicationStatus status
+    );
+}
